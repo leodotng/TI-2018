202bd3cfd4689478b2e97a373a97eb1723117f7c
leodotng <josh@leo.ng>
2018-04-13 10:57:48 -0600

    added knex and dotenv

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+// Update with your config settings.
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: {
+      database: process.env.DATABASE_NAME
+    }
+  },
+
+  production: {
+    client: "postgresql",
+    connection: process.env.DATABASE_URL
+  }
+};
