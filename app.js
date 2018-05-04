e4046e726673279c1fe9158ef3724dfc5b6470c7
leodotng <josh@leo.ng>
2018-05-04 01:24:14 -0600

    moved js to data file

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var queries = require('./database/queries');
+var data = require('./database/data');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
