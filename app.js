51fe1833ceece4fd78de5d8ded76cc88ad9bd5aa
leodotng <josh@leo.ng>
2018-06-08 17:51:06 -0600

    removed passport

--- app.js
+++ app.js
@@ -27,10 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
-app.post('/login', passport.authenticate('local', { successRedirect: '/',
-                                                    failureRedirect: '/login' }));
-
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
